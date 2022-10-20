8f40fd4b1136e4082152fc709bb9f8c3a5d80ead
andy <arteysoft@gmail.com>
2022-10-20 20:15:50 -0300

    comentarios

--- clase8/src/app/server.ts
+++ clase8/src/app/server.ts
@@ -1,115 +1,39 @@
 import express from 'express'
-import {query, insert, deleteOne, updateOne} from '../lib/mongocli2'
-import {crearObjetoFake} from '../lib/genusuario'
+import usuarioRouter from './Routes/usuario';
 
 export let ejemploCompletoExpress = () => {
     let app = express()
 
-    app.use(express.json());
+    app.use(express.json())
 
-    app.get('/inventar', (_request, response) => {
+    app.use('/', (_request, response, next) => {
         response.setHeader('Content-Type', 'application/json')
+        console.log('estoy en el middleware 1 ... seteo app json')
+        next()
+    })
 
-        let resp = crearObjetoFake()
-
-        let txtResp = JSON.stringify(resp)
+    app.use('/', (request, response, next) => {
+        console.log('estoy en el middleware 2 ... tema seguridad')
+        // voy a chequear si viene un token
+        console.log(request.header['x-token'])
+        if (request.headers['x-security-token'] !== undefined) {
+            next()
+            return
+        }
         response
-            .status(200)
-            .send(txtResp)
+            .status(401)
+            .send(JSON.stringify({mensaje:'sin credenciales'}))
     })
 
-    app.get('/', (request, response) => {
-        response.setHeader('Content-Type', 'application/json')
-        query(process.env.BASE_MONGO, 'usuarios', request.query, (e, data) => {
-            if (e) {
-                response
-                .status(500)
-                .send()        
-                return
-            }
-            response
-            .status(200)
-            .send(data)
-        })
+    app.use(usuarioRouter)
+
+    app.use('/', (err, _request, response, _next) => {
+        console.log(err)
+        response
+            .status(500)
+            .send(JSON.stringify({mensaje:'error descontrolado'}))
     })
 
-    app.get('/:id', (request, response) => {
-        response.setHeader('Content-Type', 'application/json')
-
-        let id = request.params.id
-
-        query(process.env.BASE_MONGO, 'usuarios', {id}, (e, data) => {
-            if (e) {
-                response
-                .status(500)
-                .send()        
-                return
-            }
-            if (data.length === 0) {
-                response
-                .status(404)
-                .send()        
-                return
-            }
-            response
-            .status(200)
-            .send(data[0])
-        })
-    })
-
-    app.post('/', (request, response) => {
-        response.setHeader('Content-Type', 'application/json')
-
-        let obj = request.body
-        console.log(obj)
-
-        insert(process.env.BASE_MONGO, 'usuarios', obj, (e) => {
-            if (e) {
-                response
-                .status(500)
-                .send()        
-                return
-            }
-            response
-            .status(201)
-            .send()
-        })
-    })
-
-    app.put('/:id', (request, response) => {
-        let obj = request.body
-        obj.id = request.params.id
-        console.log(obj)
-        updateOne(process.env.BASE_MONGO, 'usuarios', obj, (e) => {
-            if (e) {
-                console.log(e)
-                response
-                .status(500)
-                .send()        
-                return
-            }
-            console.log(e)
-            response
-            .status(200)
-            .send()
-        })
-    })
-
-    app.delete('/:id', (request, response) => {
-        let id = request.params.id
-        deleteOne(process.env.BASE_MONGO, 'usuarios', id, (e) => {
-            if (e) {
-                response
-                .status(500)
-                .send()        
-                return
-            }
-            console.log(e)
-            response
-            .status(200)
-            .send()
-        })
-    })
 
     app.listen(3000, () => {
         console.log('OK.. listen 3000')
